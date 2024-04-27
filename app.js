ce968367fed9d38704b06d6019c515bc132fb7f9
Jillian Tanda <jilliantanda@gmail.com>
2024-04-27 15:38:21 -0400

    evolutions working correctly

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-
-// const form = document.getElementById("form")
-// const yourName = document.getElementById("name").value
-const currentHunger = document.querySelector("#hunger");
-const currentBoredom = document.querySelector("#boredom");
-const currentSleepiness = document.querySelector("#sleepiness");
-const currentAge = document.querySelector("#age");
-const currentDay = document.querySelector("#day");
+const currentHunger = document.querySelector("#hunger")
+const currentBoredom = document.querySelector("#boredom")
+const currentSleepiness = document.querySelector("#sleepiness")
+const currentAge = document.querySelector("#age")
+const currentDay = document.querySelector("#day")
 const button1 = document.querySelector("#button1")
 const button2 = document.querySelector("#button2")
 const button3 = document.querySelector("#button3")
+const button4 = document.querySelector("#button4")
 
 
-
-let day = 0;
 
 button1.onclick = feed
 button2.onclick = sleep
-button3.onclick = playGame
+button3.onclick = playFlute
+button4.onclick = lightsOn
+
 
 
 let player = prompt("Name your elf: ", "Player name")
@@ -26,46 +24,21 @@ if (player != null){
 }
 
 
-const interval1 = setInterval(dayIncrease, 5000)
-const interval2 = setInterval(metricIncrease, 3500)
-const interval3 = setInterval(ageUP, 3000)
-
-
-
-const intervalChange = setInterval(() => {
-    if (Elf.boredom >= 20 || Elf.hunger >= 20){
-        clearInterval(interval1)
-        clearInterval(interval2)
-        clearInterval(interval3)
-        reset()
-    }
-    else if (Elf.boredom >= 10 && Elf.sleepiness >= 10){
-        clearInterval(interval1)
-        clearInterval(interval2)
-        clearInterval(interval3)
-        reset()
-    } else if (Elf.sleepiness >= 10 && Elf.hunger >= 10){
-        clearInterval(interval1)
-        clearInterval(interval2)
-        clearInterval(interval3)
-        reset ()
-    } else if (Elf.hunger >= 10 && Elf.boredom >= 10) {
-        clearInterval(interval1)
-        clearInterval(interval2)
-        clearInterval(interval3)
-        reset()
-    }
-}, 0) 
-
-
-
-const Elf = {
+const elf = {
     hunger: 0,
     sleepiness: 0,
     boredom: 0,
     age: 0
    }
 
+
+let day = 0;
+
+
+
+const interval1 = setInterval(dayIncrease, 5000)
+const interval2 = setInterval(metricIncrease, 3500)
+const interval3 = setInterval(ageUP, 3000)
 
 
 function dayIncrease(){
@@ -77,107 +50,135 @@ function dayIncrease(){
 
 function ageUP(){
     for(let i = 0; i<= 0; i++){
-        Elf.age ++
+        elf.age ++
     }
-    currentAge.innerHTML = Elf.age;
+    currentAge.innerHTML = elf.age;
+    evolution()
 }
 
-function Evolution(){
-   if (day === 2) {
-    document.getElementById('img1').src = 'baby-elf.png'
-   }
-        else if (day === 5){
-            document.getElementById('img2').src = 'evolved elf.png'
-        }
-     }
-    
+
+
 
 function metricIncrease(){
     for (let i = 0; i <= 0; i++){
-        Elf.hunger += 2
-        Elf.sleepiness += 1
-        Elf.boredom += 1
+        elf.hunger += 2
+        elf.sleepiness += 1
+        elf.boredom += 1
     }
-    currentHunger.innerHTML = Elf.hunger;
-    currentSleepiness.innerHTML = Elf.sleepiness;
-    currentBoredom.innerHTML = Elf.boredom;   
+    currentHunger.innerHTML = elf.hunger;
+    currentSleepiness.innerHTML = elf.sleepiness;
+    currentBoredom.innerHTML = elf.boredom;   
 }
         
 
 function feed(){
-    if (Elf.hunger >= 2){
-        Elf.hunger -= 2
+    if (elf.hunger >= 2){
+        elf.hunger -= 2
+        elf.sleepiness += 2
     }
-    currentHunger.innerHTML = Elf.hunger;
+    currentHunger.innerHTML = elf.hunger;
 }
 
 
 function sleep(){
-    if (Elf.sleepiness >= 3){
-        Elf.sleepiness -= 3
+    if (elf.sleepiness >= 3){
+        elf.sleepiness -= 3
+        elf.hunger += 1
     }
-    currentSleepiness.innerHTML= Elf.sleepiness;
+    currentSleepiness.innerHTML= elf.sleepiness;
+    document.body.style.backgroundImage= "url('fantasty-night.jpeg')"
 }
 
-function playGame(){
-    if (Elf.boredom >= 1){
-        Elf.boredom -= 1
-    }
-    currentBoredom.innerHTML= Elf.boredom;
+function lightsOn(){
+    document.body.style.backgroundImage= "url('morning-forest.jpeg')"
 }
 
+
+function playFlute(){
+    if (elf.boredom >= 1){
+        elf.boredom -= 1
+    }
+    currentBoredom.innerHTML= elf.boredom;
+}
+
+
+
+var egg = document.getElementById("egg")
+var babyElf = document.getElementById("baby")
+var adultElf = document.getElementById("adult")
+
+
+function evolution(){
+   if (elf.age >= 5){
+            egg.style.visibility = "hidden"
+            babyElf.style.visibility = "visible"
+   }
+  if (elf.age >= 10){
+            babyElf.style.visibility = "hidden"
+            adultElf.style.visibility = "visible"
+        }
+    }
+    
+
+
+const intervalChange = setInterval(() => {
+    if (elf.boredom >= 20 || elf.hunger >= 20){
+        clearInterval(interval1)
+        clearInterval(interval2)
+        clearInterval(interval3)
+        reset()
+    }
+    else if (elf.boredom >= 10 && elf.sleepiness >= 10){
+        clearInterval(interval1)
+        clearInterval(interval2)
+        clearInterval(interval3)
+        reset()
+    } else if (elf.sleepiness >= 10 && elf.hunger >= 10){
+        clearInterval(interval1)
+        clearInterval(interval2)
+        clearInterval(interval3)
+        reset ()
+    } else if (elf.hunger >= 10 && elf.boredom >= 10) {
+        clearInterval(interval1)
+        clearInterval(interval2)
+        clearInterval(interval3)
+        reset()
+    }
+}) 
+
+
+var deadElf = document.getElementById("dead")
 
 function reset(){
-    Elf.boredom = 0
-    Elf.hunger = 0
-    Elf.sleepiness = 0
-    Elf.age = 0
+    adultElf.style.visibility = "hidden"
+    deadElf.style.visibility = "visible"
+    elf.boredom = 0
+    elf.hunger = 0
+    elf.sleepiness = 0
+    elf.age = 0
     day = 0
-    currentHunger.innerHTML = Elf.hunger;
-    currentSleepiness.innerHTML = Elf.sleepiness;
-    currentBoredom.innerHTML = Elf.boredom;   
-    currentAge.innerHTML = Elf.age;
+    currentHunger.innerHTML = elf.hunger;
+    currentSleepiness.innerHTML = elf.sleepiness;
+    currentBoredom.innerHTML = elf.boredom;   
+    currentAge.innerHTML = elf.age;
     currentDay.innerHTML = day;
 }
 
-
-   // if boredom and sleepiness both reach 10 at the same time, character dies. 
-    // if hunger and sleepiness both reach 10 at the same time, character dies.
-    //  if hunger and boredom both reach 10 at the same time, character dies. 
-
-
-
-
-
-// Display a character of your choice on the screen to represent your pet
-
-// Display the following metrics for your pet:
-
-// Hunger (1-10 scale)
-// Sleepiness (1-10 scale)
-// Boredom (1-10 scale)
-// Age
-// const 
 
 
 
 // Create buttons to:
     // -feed your pet, -2 per feed berries
+        //- +2 sleepiness
     //- make pet sleep  -3
-    // - make pet play board game, -1 boredom
+        //- 1 hunger
+    // - make pet play the flute, -1 boredom
 
 
 // Add the ability to name your pet.
 
     // Submit box- Welcome to the forest. Please select your characters name
     // Display character name on page. 
-// setInterval(metricIncrease, 10000)
-
-//Display rules-
-
-
-
-
 
 
     // every 10 seconds increases hunger by 2. If 20 is reached, character dies.
@@ -192,13 +193,6 @@ function reset(){
 // Style the page.
 
 
-// Increase your pet's age every x seconds
-
-
-// Morph your pet at certain ages.
-// start at age 0- creature is an egg-
-// age 5- creature is a baby-
-// age 15- creature is an adult
 
 
 // Animate your pet across the screen while it's alive.
